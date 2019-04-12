fb1591eb9da9946162c0681888e8dab4265624cf
Marvin <18121196@CNSUNING.COM>
2019-04-12 10:26:27 +0800

    优化测试用例

--- test.js
+++ test.js
@@ -11,8 +11,10 @@ console.log(JSON.stringify(jsonx.parse(`callback({
 `), null, '    '))
 
 console.log(xmlx.parse(`
+s-for="item, index in block.sugGoods"
 <text disabled a=1 b=2 data-city='{{util.cal("a", 1)}}'>
 <!-- comment -->
+<aabb>s-for="item, index in block.sugGoods"</aabb>
 fdasfasdadf{{couponInfo.parValue('abc', 1)}}<button disabled />
 </text>
 `, { format: true, indent: '    ' }).toString());
