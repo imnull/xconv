53df34fd05bcddd10f183a93332d8e4f4a9cdeb8
Marvin <imnull@outlook.com>
2019-09-04 13:29:38 +0800

    update version/git/test

--- test.js
+++ test.js
@@ -11,25 +11,56 @@ const { jsonx, xmlx, Reader, StyleReader } = require('./index');
 // `), null, '    '))
 
 console.log(xmlx.parse(`
-<image a="1{{{a:1}}}aaa{{{a:2}}}123" src="asdfas{{imgHost}}/uimg/ZR/share_order/{{item.url}}_400x400.jpg"></image>
+<image b="{{C.sub(1,'}}')}}"
+  a="1{{{a:1}}}aaa{{{a:2}}}123"
+  src="asdfas{{imgHost}}/uimg/ZR/share_order/{{item.url}}_400x400.jpg">
+  </image >
+
 s-for="item, index in block.sugGoods"
+
 <text disabled a=1 b=2 data-city='abc-{{ 123 }}-abc-1234-{{ util.cal("a",1) }}'>
-<!-- comment -->
+   <!-- comment -->
 <aabb>
   ¥
-  {{couponInfo.parValue('abc', 1)}}
+  {{couponInfo2222.parValue('abc', 1, '{{}}')}}
 </aabb>
-<a><b><c/></b></a>
-<button disabled /><view>{{couponInfo.parValue('abc', 1)}}</view>
+<a><b><c/></b>
+  </a>
+<button disabled />  <view>{{couponInfo.parValue('abc', 1)}}</view>
 </text>
 `, {
     NSGlobal: 'wx',
     NSSpliter: ':',
-    format: true,
-    indent: '    ',
-    formatIgnore: [3,101],
-    aloneElements: ['button', 'input']
+    // format: false,
+    // indent: '  ',
+    // formatIgnore: [3,101],
+    // aloneElements: ['button', 'input']
 }).toString({ NSGlobal: 's', NSSpliter: '-' }));
+
+const x = xmlx.parse(`
+<image b="{{C.sub(1,'}}')}}"
+  a="1{{{a:1}}}aaa{{{a:2}}}123"
+  src="asdfas{{imgHost}}/uimg/ZR/share_order/{{item.url}}_400x400.jpg">
+  </image >
+
+s-for="item, index in block.sugGoods"
+
+<text disabled a=1 b=2 data-city='abc-{{ 123 }}-abc-1234-{{ util.cal("a",1) }}'>
+   <!-- comment -->
+<aabb>
+  ¥
+  {{couponInfo2222.parValue('abc', 1, '{{}}')}}
+       <a><b><c/></b>
+  </a>
+</aabb>
+      <a><b><c/></b>
+  </a>
+<button disabled />  <view>{{couponInfo.parValue('abc', 1)}}</view>
+</text>
+`)
+
+console.log(1111111)
+console.log(x.query(n => n.name === 'aabb').toString())
 
 // let r = new StyleReader(`
 // body {
